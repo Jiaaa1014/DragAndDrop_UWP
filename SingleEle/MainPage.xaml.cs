148407abac3aafb4dac42966950190c6bcee7060
jia0 <jia0@dentall.io>
2018-08-02 17:04:43 +0800

    init

---
+++ SingleEle/MainPage.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Mime;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json;
+using Windows.UI.Xaml.Media.Imaging;
+using Color = Windows.UI.Color;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SingleEle
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Trigger_OnClick(object sender, RoutedEventArgs e)
+        {
+            UserAvatar.ProfilePicture = new BitmapImage(new Uri("ms-appx:///Assets/alex-bertha-134419-unsplash.jpg"));
+        }
+
+        private void PlaceToDragDrop_OnDragOver(object sender, DragEventArgs e)
+        {
+            e.AcceptedOperation = DataPackageOperation.Copy;
+
+            if (e.DragUIOverride == null) return;
+            e.DragUIOverride.Caption = "drop to create a custom sound and tile";
+            e.DragUIOverride.IsCaptionVisible = true;
+            e.DragUIOverride.IsContentVisible = true;
+            e.DragUIOverride.IsGlyphVisible = true;
+
+            PlaceToDragDrop.Background = new SolidColorBrush(Color.FromArgb(255,120,120,120));
+        }
+
+        private async void PlaceToDragDrop_OnDrop(object sender, DragEventArgs e)
+        {
+            // HACK if drag non-denfined type file, it crashed.
+
+
+            if (e.DataView.Contains(StandardDataFormats.StorageItems))
+            {
+                var items = await e.DataView.GetStorageItemsAsync();
+                if (items.Any())
+                {
+                    var storageFile = items[0] as StorageFile;
+                    var contentType = storageFile.ContentType;
+
+                    if (contentType == "image/jpeg" || contentType =="image/png")
+                    {
+                        var bitmapImage = new BitmapImage();
+                        bitmapImage.SetSource(await storageFile.OpenAsync(FileAccessMode.Read));
+                        // Set the image on the main page to the dropped image
+                        UserAvatar.ProfilePicture = bitmapImage;
+                    }
+                }
+            }
+        }
+
+        private async void Upload_Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker openPicker = new FileOpenPicker
+            {
+                ViewMode = PickerViewMode.Thumbnail,
+                SuggestedStartLocation = PickerLocationId.DocumentsLibrary
+            };
+            openPicker.FileTypeFilter.Add(".jpg");
+            openPicker.FileTypeFilter.Add(".jpeg");
+            openPicker.FileTypeFilter.Add(".png");
+
+            StorageFile file = await openPicker.PickSingleFileAsync();
+            if (file == null)
+            {
+                return;
+            }
+            using (var fileStream = await file.OpenAsync(FileAccessMode.Read))
+            {
+                var bitmapImage = new BitmapImage();
+                bitmapImage.SetSource(fileStream);
+                // Set the image on the main page to the dropped image
+                UserAvatar.ProfilePicture = bitmapImage;
+            }
+        }
+
+        private void PlaceToDragDrop_OnDragLeave(object sender, DragEventArgs e)
+        {
+            PlaceToDragDrop.Background = new SolidColorBrush(Color.FromArgb(255, 190, 190, 190));
+        }
+    }
+}
